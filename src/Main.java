ec5bf0988836d784271219bf638c5c24e23460b9
Haridharshini <haridharshini.j@gmail.com>
2025-08-16 19:28:01 +0530

    Last commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 public class Main {
     public static void main(String[] args){
+        //headsOrTails
         Random ran = new Random();
         boolean isHeads = ran.nextBoolean();
         if(isHeads){
